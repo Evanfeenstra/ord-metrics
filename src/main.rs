4bf966adea3c1bd69681f9f9c86636b8a70406a3
Evan Feenstra <evanfeenstra@gmail.com>
2024-01-25 19:01:40 -0800

    count and log

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,26 @@
-use std::any;
+use std::{any, os::macos::raw::stat};
 
 use serde::{Deserialize, Serialize};
 use std::collections::BTreeMap;
 
 const ROOT: &str = "http://0.0.0.0:3000";
 
+const SNAPSHOT_HEIGHT: u32 = 826600;
+
 type Result<T> = std::result::Result<T, anyhow::Error>;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Stats {
     any: u64,
-    img: u64,
+    cool: u64,
     boring: u64,
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    // let id1 = "6fb976ab49dcec017f1e201e84395983204ae1a7c2abf7ced0a85d692e442799i0";
-    // let url1 = format!("inscription/{}", id1);
-    // let resp = get(&url1).await?;
-    // println!("{}", resp);
-
     let mut ret = BTreeMap::<String, Stats>::new();
 
     // let first_block = 767430;
-    // let end_block = first_block + 100;
     // let snapshot_block = 826600;
 
     let mut page = 0;
@@ -36,9 +32,122 @@ async fn main() -> Result<()> {
         page += 1;
     }
 
-    println!("ret: {:?}", ret);
+    // println!("ret: {:?}", ret);
+
+    count_addresses(&ret);
 
     Ok(())
+}
+
+fn count_addresses(ret: &BTreeMap<String, Stats>) {
+    /*
+    -Holds 2 or more inscriptions of any file type
+    -Holds 3 or more inscriptions of any file type
+    -Holds 4 or more inscriptions of any file type
+    -Holds 5 or more inscriptions of any file type
+    -Holds 2 or more inscriptions that are not the file types text/plain, text/plain;charset=utf-8, or application/json
+    -Holds 3 or more inscriptions that are not the file types text/plain, text/plain;charset=utf-8, or application/json
+    -Holds 4 or more inscriptions that are not the file types text/plain, text/plain;charset=utf-8, or application/json
+    -Holds 5 or more inscriptions that are not the file types text/plain, text/plain;charset=utf-8, or application/json
+    -Holds 10 or more inscriptions that are either text/plain, text/plain;charset=utf-8, or application/json
+    -Holds 25 or more inscriptions that are either text/plain, text/plain;charset=utf-8, or application/json
+    -Holds 50 or more inscriptions that are either text/plain, text/plain;charset=utf-8, or application/json
+    */
+    let mut count_2_or_more = 0;
+    let mut count_3_or_more = 0;
+    let mut count_4_or_more = 0;
+    let mut count_5_or_more = 0;
+    let mut count_2_or_more_cool = 0;
+    let mut count_3_or_more_cool = 0;
+    let mut count_4_or_more_cool = 0;
+    let mut count_5_or_more_cool = 0;
+    let mut count_10_or_more_boring = 0;
+    let mut count_25_or_more_boring = 0;
+    let mut count_50_or_more_boring = 0;
+
+    for (_addy, stats) in ret {
+        //
+        if stats.any >= 2 {
+            count_2_or_more += 1;
+        }
+        if stats.any >= 3 {
+            count_3_or_more += 1;
+        }
+        if stats.any >= 4 {
+            count_4_or_more += 1;
+        }
+        if stats.any >= 5 {
+            count_5_or_more += 1;
+        }
+        //
+        if stats.cool >= 2 {
+            count_2_or_more_cool += 1;
+        }
+        if stats.cool >= 3 {
+            count_3_or_more_cool += 1;
+        }
+        if stats.cool >= 4 {
+            count_4_or_more_cool += 1;
+        }
+        if stats.cool >= 5 {
+            count_5_or_more_cool += 1;
+        }
+        //
+        if stats.boring >= 10 {
+            count_10_or_more_boring += 1;
+        }
+        if stats.boring >= 25 {
+            count_25_or_more_boring += 1;
+        }
+        if stats.boring >= 50 {
+            count_50_or_more_boring += 1;
+        }
+    }
+
+    println!(
+        "2 or more inscriptions of any file type: {}",
+        count_2_or_more
+    );
+    println!(
+        "3 or more inscriptions of any file type: {}",
+        count_3_or_more
+    );
+    println!(
+        "4 or more inscriptions of any file type: {}",
+        count_4_or_more
+    );
+    println!(
+        "5 or more inscriptions of any file type: {}",
+        count_5_or_more
+    );
+    println!(
+        "2 or more inscriptions that are not the file types text/plain, text/plain;charset=utf-8, or application/json: {}",
+        count_2_or_more_cool
+    );
+    println!(
+        "3 or more inscriptions that are not the file types text/plain, text/plain;charset=utf-8, or application/json: {}",
+        count_3_or_more_cool
+    );
+    println!(
+        "4 or more inscriptions that are not the file types text/plain, text/plain;charset=utf-8, or application/json: {}",
+        count_4_or_more_cool
+    );
+    println!(
+        "5 or more inscriptions that are not the file types text/plain, text/plain;charset=utf-8, or application/json: {}",
+        count_5_or_more_cool
+    );
+    println!(
+        "10 or more inscriptions that are either text/plain, text/plain;charset=utf-8, or application/json: {}",
+        count_10_or_more_boring
+    );
+    println!(
+        "25 or more inscriptions that are either text/plain, text/plain;charset=utf-8, or application/json: {}",
+        count_25_or_more_boring
+    );
+    println!(
+        "50 or more inscriptions that are either text/plain, text/plain;charset=utf-8, or application/json: {}",
+        count_50_or_more_boring
+    );
 }
 
 async fn process_inscriptions_page(page: u64, ret: &mut BTreeMap<String, Stats>) -> Result<bool> {
@@ -51,8 +160,15 @@ async fn process_inscriptions_page(page: u64, ret: &mut BTreeMap<String, Stats>)
         page,
         block.inscriptions.len()
     );
+
+    let mut is_more = block.more;
+
     for insc in block.inscriptions {
         let i = get_inscription(&insc).await?;
+        if i.genesis_height > SNAPSHOT_HEIGHT {
+            is_more = false;
+            break;
+        }
         if let Some(addy) = i.address {
             if let Some(mime) = i.content_type {
                 match ret.get_mut(&addy) {
@@ -60,18 +176,18 @@ async fn process_inscriptions_page(page: u64, ret: &mut BTreeMap<String, Stats>)
                         r.any += 1;
                         match is_boring(&mime) {
                             true => r.boring += 1,
-                            false => r.img += 1,
+                            false => r.cool += 1,
                         };
                     }
                     None => {
                         let mut sta = Stats {
                             any: 1,
-                            img: 0,
+                            cool: 0,
                             boring: 0,
                         };
                         match is_boring(&mime) {
                             true => sta.boring += 1,
-                            false => sta.img += 1,
+                            false => sta.cool += 1,
                         };
                         ret.insert(addy.clone(), sta);
                     }
@@ -79,7 +195,7 @@ async fn process_inscriptions_page(page: u64, ret: &mut BTreeMap<String, Stats>)
             }
         }
     }
-    Ok(block.more)
+    Ok(is_more)
 }
 
 fn is_boring(mime: &str) -> bool {
@@ -88,6 +204,7 @@ fn is_boring(mime: &str) -> bool {
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct InscriptionJson {
+    genesis_height: u32,
     address: Option<String>,
     content_type: Option<String>,
 }
