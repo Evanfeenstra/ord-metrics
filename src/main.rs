69d8225b16c1e2ae0cab49aa4486f4a40492b687
Evan Feenstra <evanfeenstra@gmail.com>
2024-01-25 18:42:34 -0800

    return map of addresses to ords

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,81 @@
+use std::any;
+
 use serde::{Deserialize, Serialize};
+use std::collections::BTreeMap;
 
 const ROOT: &str = "http://0.0.0.0:3000";
 
 type Result<T> = std::result::Result<T, anyhow::Error>;
 
+#[derive(Serialize, Deserialize, Debug)]
+struct Stats {
+    any: u64,
+    img: u64,
+    boring: u64,
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
-    let id1 = "6fb976ab49dcec017f1e201e84395983204ae1a7c2abf7ced0a85d692e442799i0";
-    let url1 = format!("inscription/{}", id1);
-    let resp = get(&url1).await?;
-    println!("{}", resp);
+    // let id1 = "6fb976ab49dcec017f1e201e84395983204ae1a7c2abf7ced0a85d692e442799i0";
+    // let url1 = format!("inscription/{}", id1);
+    // let resp = get(&url1).await?;
+    // println!("{}", resp);
 
-    let first_block = 767430;
-    let end_block = first_block + 100;
-    let snapshot_block = 826600;
+    let mut ret = BTreeMap::<String, Stats>::new();
 
-    for bn in first_block..end_block {
-        get_inscriptions_in_block(bn).await?;
+    // let first_block = 767430;
+    // let end_block = first_block + 100;
+    // let snapshot_block = 826600;
+
+    let mut page = 0;
+    loop {
+        let is_more = process_inscriptions_page(page, &mut ret).await?;
+        if !is_more || page > 10 {
+            break;
+        }
+        page += 1;
     }
 
-    for page in 0..100 {
-        get_inscriptions_page(page).await?;
-    }
+    println!("ret: {:?}", ret);
 
     Ok(())
 }
 
-async fn get_inscriptions_page(page: u64) -> Result<()> {
+async fn process_inscriptions_page(page: u64, ret: &mut BTreeMap<String, Stats>) -> Result<bool> {
     let url = format!("inscriptions/{}", page);
     let resp = get(&url).await?;
     let block: InscriptionsJson = serde_json::from_str(&resp)?;
-    if block.more {
-        println!("=======> page {} has more inscriptions", page);
-    }
+
     println!(
         "=> page {} has {} inscriptions",
         page,
         block.inscriptions.len()
     );
-    Ok(())
+    for insc in block.inscriptions {
+        let i = get_inscription(&insc).await?;
+        match ret.get_mut(&i.address) {
+            Some(r) => {
+                r.any += 1;
+            }
+            None => {
+                ret.insert(
+                    i.address.clone(),
+                    Stats {
+                        any: 1,
+                        img: 0,
+                        boring: 0,
+                    },
+                );
+            }
+        }
+    }
+    Ok(block.more)
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct InscriptionJson {
+    address: String,
+    content_type: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -47,6 +84,13 @@ pub struct InscriptionsJson {
     more: bool,
     page_index: u64,
 }
+
+async fn get_inscription(id: &str) -> Result<InscriptionJson> {
+    let url = format!("inscription/{}", id);
+    let resp = get(&url).await?;
+    Ok(serde_json::from_str(&resp)?)
+}
+
 async fn get_inscriptions_in_block(bn: u64) -> Result<()> {
     let url = format!("inscriptions/block/{}", bn);
     let resp = get(&url).await?;
